8206fe084ef87f922326118c528fc7d8b531583b
sya-ri <sya79lua@gmail.com>
2020-04-27 21:06:27 +0900

    displayに変更を加えました

--- src/main/kotlin/me/syari/ss/battle/status/player/StatusType.kt
+++ src/main/kotlin/me/syari/ss/battle/status/player/StatusType.kt
@@ -6,13 +6,13 @@ import me.syari.ss.battle.equipment.ElementType
  * ステータスの種類
  */
 sealed class StatusType(val display: String) {
-    object BaseAttack : StatusType("攻撃力")
-    object BaseDefense : StatusType("防御力")
-    data class ElementAttack(val elementType: ElementType) : StatusType("属性攻撃力 ${elementType.display}")
-    data class ElementDefense(val elementType: ElementType) : StatusType("属性防御力 ${elementType.display}")
-    object CriticalChance : StatusType("会心率")
-    object MaxDamage : StatusType("最大ダメージ")
-    object MaxHealth : StatusType("最大体力")
-    object RegenHealth : StatusType("体力回復")
-    object MoveSpeed : StatusType("移動速度")
+    object BaseAttack : StatusType("&6攻撃力")
+    object BaseDefense : StatusType("&6防御力")
+    data class ElementAttack(val elementType: ElementType) : StatusType("&6属性攻撃力≪${elementType.display}&6≫")
+    data class ElementDefense(val elementType: ElementType) : StatusType("&6属性防御力≪${elementType.display}&6≫")
+    object CriticalChance : StatusType("&6会心率")
+    object MaxDamage : StatusType("&6最大ダメージ")
+    object MaxHealth : StatusType("&6最大体力")
+    object RegenHealth : StatusType("&6体力回復")
+    object MoveSpeed : StatusType("&6移動速度")
 }
